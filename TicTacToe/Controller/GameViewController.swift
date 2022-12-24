7303b96c5e95b9efa57dcfba6e367a7bed05ef60
noelpettersson <90799107+noelpettersson@users.noreply.github.com>
2022-12-24 11:01:17 -0800

    Initial commit
    Storyboard already added

---
+++ TicTacToe/Controller/GameViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GameViewController.swift
+//  TicTacToe
+//
+//  Created by Noel Pettersson on 2022-12-24.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    //First row of grid buttons
+    @IBOutlet var a1: UIButton!
+    @IBOutlet var a2: UIButton!
+    @IBOutlet var a3: UIButton!
+    
+    //Second row of grid buttons
+    @IBOutlet var b1: UIButton!
+    @IBOutlet var b2: UIButton!
+    @IBOutlet var b3: UIButton!
+    
+    //Third row of grid buttons
+    @IBOutlet var c1: UIButton!
+    @IBOutlet var c2: UIButton!
+    @IBOutlet var c3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ TicTacToe/Model/Game.swift
@@ -0,0 +1,8 @@
+//
+//  Game.swift
+//  TicTacToe
+//
+//  Created by Noel Pettersson on 2022-12-24.
+//
+
+import Foundation
